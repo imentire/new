326039fec340f53d9eeb67c11b968567c5499f1e
Юлия Азарина <imentire@gmail.com>
2017-06-23 00:22:40 +0300

    activities блок, confidence слайдер

--- frontend/src/_scripts/main.js
+++ frontend/src/_scripts/main.js
@@ -3,10 +3,36 @@
 
 'use strict';
 
-var $ = require('jquery');
+var $ = window.jQuery = require('jquery');
+require('owl.carousel');
 var Link = require('../_modules/link/link');
 
 $(function() {
   new Link(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
+});
+
+$(function() {
+    var owlCustomer = $('.js-confidence-carousel');
+    owlCustomer.owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            940: {
+                items: 4
+            }
+        }
+    });
+
+    $('.js-confidence-carousel-next').on('click', function() {
+        owlCustomer.trigger('next.owl.carousel');
+    });
+    $('.js-confidence-carousel-prew').on('click', function() {
+        owlCustomer.trigger('prev.owl.carousel');
+    });
 });
